3ab05b3d73f589f107adff227dafac58a6bb44e8
Lokesh <lokeshgadiya177@gmail.com>
2023-11-08 10:36:49 +0530

    Initial Structure

---
+++ src/main/java/CoreJava/Person.java
@@ -0,0 +1,37 @@
+//An Actual Person
+//    |
+//Resides in Person.java
+//    |
+//Converted into Person.class using Java Compiler
+
+
+//Structure of Java
+/*  ■ The package statement
+        1. Should be first statement.
+        2. Cannot have multiple package in same class
+ */
+package CoreJava;
+/*  ■ The import statement
+        1. Import statement must follow package statement.
+*/
+
+class Person {
+      //    ■ Class is design used to specify properties and behaviour of the object.
+
+
+      //    ■ Constructors(Create the object)
+      Person(){
+
+      }
+      //    ■ Variables (Properties)
+      int integerVar ;
+
+      //    ■ Methods (Behaviour)
+      int getIntegerVar(){
+          return integerVar;
+      }
+
+//    ■ Comments
+
+
+}
