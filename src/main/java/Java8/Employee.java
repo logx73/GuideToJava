89fe2fb9c5f51d45fb3bd2c762a3a09ee7567a72
lokesh <lokeshgadiya177@gmail.com>
2024-01-08 23:46:37 +0530

    Java 8 problem solving

---
+++ src/main/java/Java8/Employee.java
@@ -0,0 +1,47 @@
+package Java8;
+
+public class Employee {
+    private int id;
+    private String name;
+    private int salary;
+
+    public Employee(int id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+//    @Override
+//    public int compareTo(Object o) {
+//        Employee employee = (Employee) o;
+//        if(this.getSalary() > employee.getSalary()){
+//            return 1;
+//        }else {
+//            return -1;
+//        }
+//    }
+}
--- src/main/java/Java8/streams.java
+++ src/main/java/Java8/streams.java
@@ -3,19 +3,62 @@ package Java8;
 import java.util.*;
 import java.util.stream.Collectors;
 
+
+
 public class streams {
+    @FunctionalInterface
+    interface SumOfIntegers {
+        int sum(int a, int b);
+    }
+
     public static void main(String[] args) {
-        List<Integer> integerList = Arrays.asList(1,2,3,4,5,6);
-        List<String> stringList = Arrays.asList("wow","Animesh","Akshay");
-        stringList.stream().filter(str->str.startsWith("A")).forEach(System.out::println);
-        stringList.stream().sorted().forEach(System.out::println);
-        System.out.println(stringList.stream().filter(str->str.length()>5).count());
-        System.out.println(stringList.stream().reduce((x,y)->x.length()>y.length()?x:y).get());
-        integerList.stream().filter(num->num%2!=0).forEach(System.out::println);
-        System.out.println(integerList.stream().mapToInt(i->i).reduce((x,y)->x*y).getAsInt());
-        System.out.println("Max number from integer list" + integerList.stream().mapToInt(e->e).average());
-        Map<Character, Long> collect1 = "lolkesh".chars().mapToObj(i -> (char) i).collect(Collectors.groupingBy(i -> i, Collectors.counting())).entrySet().stream().filter(map->map.getValue()>1).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-        System.out.println("2nd highest element"+integerList.stream().sorted(Comparator.reverseOrder()).mapToInt(i->i).skip(1).findFirst());
-        stringList.stream().filter(str->str.contentEquals(new StringBuilder(str).reverse())).forEach(System.out::println);
+        //Dummy Dataset
+        Employee e1 = new Employee(1, "Lokesh", 75000);
+        Employee e2 = new Employee(2, "Pritesh", 25000);
+        Employee e3 = new Employee(3, "Mitesh", 50000);
+
+        // Find Second-Highest Salary from the list of Employee
+        List<Employee> employeeList = Arrays.asList(e1, e2, e3);
+        Optional<Employee> employee = employeeList.stream().sorted(Comparator.comparingInt(Employee::getSalary)).skip(1).findFirst();
+        System.out.println("Second Highest Salary " + employee.get().getName() + " Rs " + employee.get().getSalary());
+
+        // Lambda Expression that takes sum of two integers and return their sum.
+        SumOfIntegers sumFunction = Integer::sum;
+        int total = sumFunction.sum(2, 3);
+        System.out.println("Sum from lambda expression " + total);
+
+        //Sum of all even number's in the list
+        int[] listOfint = {1, 2, 3, 4, 5, 6, 7};
+        int sumOfEven = Arrays.stream(listOfint).filter(num -> num % 2 == 0).sum();
+        System.out.println("Sum Of all even numbers " + sumOfEven);
+
+        //List all the strings starting with P
+        List<String> stringList = Arrays.asList("Lokesh", "Mitesh", "Pritesh", "Gitesh", "Gaurav", "Parag");
+        List<String> filteredStringList = stringList.stream().filter(str -> str.startsWith("P")).collect(Collectors.toList());
+        System.out.println("List of String Starting with P" + filteredStringList.toString());
+
+        //Sort the arraylist alphabetically
+        List<String> sortedStringList = stringList.stream().sorted().collect(Collectors.toList());
+        System.out.println("Alphabetically sorted list " + sortedStringList.toString());
+
+        //Find Duplicate String in an array
+        String[] str = {"wow", "wow", "app"};
+
+        //HashMap Version
+        Map<String, Integer> mapOfStrings = new HashMap<>();
+        List<String> strList = new ArrayList<>();
+        for (int i = 0; i < str.length; i++) {
+            if (mapOfStrings.containsKey(str[i])) {
+                strList.add(str[i]);
+                mapOfStrings.put(str[i], mapOfStrings.get(str[i]) + 1);
+            } else {
+                mapOfStrings.put(str[i], 1);
+            }
+        }
+        System.out.println("Hashmap version duplicate list: " + strList);
+
+        //Java 8 Version
+        List<String> arryList = Arrays.stream(str).collect(Collectors.groupingBy(st -> st, Collectors.counting())).entrySet().stream().filter(map -> map.getValue() > 1).map(map -> map.getKey()).collect(Collectors.toList());
+        System.out.println("Java8 version duplicate list: " + arryList);
     }
 }
